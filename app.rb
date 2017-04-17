99005fc4d1a51a7273b10808974212f51c36b6de
Caroline Chen <carolinecaroline@gmail.com>
2017-04-17 15:16:58 -0400

    create files needed for pokemon, can list all pokemons

---
+++ app.rb
@@ -0,0 +1,13 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require 'active_record'
+
+
+require_relative 'db/connection'
+# require_relative 'models/trainer'
+require_relative 'models/pokemon'
+
+get '/pokemons' do
+	@pokemons = Pokemon.all
+	erb :"pokemons/index"
+end
---
+++ db/schema.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS pokemons;
+
+CREATE TABLE pokemons (
+id SERIAL PRIMARY KEY,
+name VARCHAR,
+poke_type VARCHAR,
+cp INTEGER,
+img_url VARCHAR
+);
