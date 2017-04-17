5678b8d69ad0e568980e954c29e5743629e37320
Caroline Chen <carolinecaroline@gmail.com>
2017-04-17 16:15:17 -0400

    can create new pokemon

--- app.rb
+++ app.rb
@@ -7,13 +7,31 @@ require_relative 'db/connection'
 # require_relative 'models/trainer'
 require_relative 'models/pokemon'
 
+get'/' do
+	erb :"pokemons/menu"
+end
+
 get '/pokemons' do
 	@pokemons = Pokemon.all
 	erb :"pokemons/index"
 end
 
+get '/pokemons/new' do
+  erb :"pokemons/new"
+end
 
 get '/pokemons/:id' do
 	@pokemon = Pokemon.find(params[:id])
 	erb :"pokemons/show"
 end
+
+get "/pokemons/:id/edit" do
+  @pokemon = Pokemon.find(params[:id])
+  erb(:"pokemons/edit")
+end
+
+post '/pokemons' do
+  @pokemon = Pokemon.create(name: params[:name], poke_type: params[:poke_type], cp: params[:cp], img_url: params[:img_url])
+  redirect "/pokemons/#{@pokemon.id}"
+end
+
